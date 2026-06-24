7b8db705179ba2cf2b6ed932d42b0222cbfbf8ae
Palombo2001 <148081701+Palombo2001@users.noreply.github.com>
2026-06-24 17:44:07 +0200

    CORREZIONE

--- api/analyze.ts
+++ api/analyze.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { analyzeCognitiveText } from "../src/utils/cognitiveEngine";
+import { analyzeCognitiveText } from "../src/utils/cognitiveEngine.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Only allow POST requests
